675a5626539bd5fdda10c58de8784ddafbeea5d1
Jose Arcidiacono <j2arcidiacono@outlook.com>
2021-05-26 16:53:24 -0300

    typo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ const HomeScreen = () => (
     <div>
         <p>
             Esta es una colección de clientes para distintos protocolos utilizados para obtención, depósito e intercambio de metadatos en repositorios.<br/>
-            Se puede cada cliente en el menú superior.
+            Se puede acceder a cada cliente en el menú superior.
         </p>
     </div>
 )
