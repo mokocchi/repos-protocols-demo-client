0665c91cc91f3e6c0b59ff8fe201bd0dbe282bc3
Jose Arcidiacono <j2arcidiacono@outlook.com>
2021-05-26 16:54:42 -0300

    error en link

--- src/components/main/Menu.js
+++ src/components/main/Menu.js
@@ -7,16 +7,16 @@ const Menu = () => (
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/#/oai-pmh"><span className={"nav-text"}>OAI-PMH</span></Nav.Link>
+                    <Nav.Link href="/repos-protocols-demo-client/#/oai-pmh"><span className={"nav-text"}>OAI-PMH</span></Nav.Link>
                 </Nav>
                 <Nav className="mr-auto">
-                    <Nav.Link href="/#/open-search"><span className={"nav-text"}>OpenSearch</span></Nav.Link>
+                    <Nav.Link href="/repos-protocols-demo-client/#/open-search"><span className={"nav-text"}>OpenSearch</span></Nav.Link>
                 </Nav>
                 <Nav className="mr-auto">
-                    <Nav.Link href="/#/sru"><span className={"nav-text"}>SRU</span></Nav.Link>
+                    <Nav.Link href="/repos-protocols-demo-client/#/sru"><span className={"nav-text"}>SRU</span></Nav.Link>
                 </Nav>
                 <Nav className="mr-auto">
-                    <Nav.Link href="/#/sword"><span className={"nav-text"}>SWORD</span></Nav.Link>
+                    <Nav.Link href="/repos-protocols-demo-client/#/sword"><span className={"nav-text"}>SWORD</span></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
